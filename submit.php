79d709f9bb61a12e9d5275fc71817776eb56f737
blackark87 <blackark87@gmail.com>
2015-06-05 22:29:47 +0900

    Update submit.php
    전체 수정

--- submit.php
+++ submit.php
@@ -1,17 +1,24 @@
 <?php
 require_once './db.php';
-header("Content-Type: text/html; charset=UTF-8");
 
 $id = $_POST['id'];
 $passwd = $_POST['passwd'];
 $name = $_POST['name'];
 $mac = $_POST['mac'];
+$major = $_POST['major'];
 
 $db = new DBC;
 $db->DBI();
 
-$db->query = "insert into user VALUES (".$id.",'".$name."',PASSWORD(".$passwd."),'".$mac."',  0)";
+$db->query = "insert into user VALUES (".$id.",'".$name."',PASSWORD(".$passwd."),'".$mac."','0','".$major."')";
 $db->DBQ();
-
 $db->DBO();
+$result = $db->result;
+
+if($result == 1){
+	//$query = "insert into chul_check(student_number,student_name,mac_address) values(".$id.",'".$name."','".$mac."')";
+	//mysql_query($query);
+	echo "0";
+}
+else{echo "1";}
 ?>
