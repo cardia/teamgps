7c11b4ddc32ea73f316ea10ab59b3d6d4e89289e
blackark87 <blackark87@gmail.com>
2015-06-06 23:25:17 +0900

    Update lecture_add.php
    강의 날자 달력으로 선택 가능(월 바꿈 안됨 / 현재 6월 고정인 상태)
    학생 추가 파트 구현해야 함

--- lecture_add.php
+++ lecture_add.php
@@ -1,34 +1,77 @@
 <?php
-include('./admin_privilege.php');
-include('./db_conn.php');
+require_once './db.php';
+require_once './admin_privilege.php';
 
 function lecture_list(){
-	$query_result = mysql_query("select title from lecture");
+	$db = new DBC;
+	$db->DBI();
+	$db->query = "select title,lecture_number from lecture";
+	$db->DBQ();
+	$db->DBO();
 	echo "<select id='title' name='title'>\n";
-	while($row = @mysql_fetch_array($query_result)){
-		echo "\t<option value='".$row[title]."'>".$row[title]."</option>\n";
+	while($row = $db->result->fetch_assoc()){
+		echo "\t<option value='".$row[lecture_number]."'>".$row[title]."(".$row[lecture_number].")</option>\n";
 	}
 	echo "</select>\n<p/>\n";
 }
 
-function student_list(){
-	$query_result = mysql_query("select student_number, student_name, lecture_1, lecture_2, lecture_3, lecture_4, lecture_5, lecture_6 from student_info");
+/*function student_list(){
+	$db = new DBC;
+	$db->DBI();
+	$db->query = "select u.user_number, u.user_name l.title from user as u, lecture as l";
+	$db->DBQ();
+	$db->DBO();
 	echo "<table border='1'>\n";
-	echo "\t<tr>\n\t\t<td></td>\n\t\t<td>학번</td>\n\t\t<td>이름</td>\n\t\t<td>과목1</td>\n\t\t<td>과목2</td>\n\t\t<td>과목3</td>\n\t\t<td>과목4</td>\n\t\t<td>과목5</td>\n\t\t<td>과목6</td>\n\t</tr>\n";
-	while($row = @mysql_fetch_array($query_result)){
-		echo "\t<tr>\n\t\t<td><input type='checkbox' name='checkbox[]' id='checkbox[]' value='".$row[student_number]."'></td>\n";
-		echo "\t\t<td>".$row[student_number]."</td>\n";
-		echo "\t\t<td>".$row[student_name]."</td>\n";
-		echo "\t\t<td>".$row[lecture_1]."</td>\n";
-		echo "\t\t<td>".$row[lecture_2]."</td>\n";
-		echo "\t\t<td>".$row[lecture_3]."</td>\n";
-		echo "\t\t<td>".$row[lecture_4]."</td>\n";
-		echo "\t\t<td>".$row[lecture_5]."</td>\n";
-		echo "\t\t<td>".$row[lecture_6]."</td>\n\t</tr>\n";
-		
+	//echo "\t<tr>\n\t\t<td></td>\n\t\t<td>학번</td>\n\t\t<td>이름</td>\n\t\t<td>과목1</td>\n\t\t<td>과목2</td>\n\t\t<td>과목3</td>\n\t\t<td>과목4</td>\n\t\t<td>과목5</td>\n\t\t<td>과목6</td>\n\t</tr>\n";
+	echo "\t<tr>\n\t\t<td></td>\n\t\t<td>학번</td>\n\t\t<td>이름</td>\n\t</tr>\n";
+	while($row = $db->result->fetch_assoc()){
+		echo "\t<tr>\n\t\t<td><input type='checkbox' name='checkbox[]' id='checkbox[]' value='".$row[user_number]."'></td>\n";
+		echo "\t\t<td>".$row[user_number]."</td>\n";
+		echo "\t\t<td>".$row[user_name]."</td>\n\t</tr>\n";
 	}
-	mysql_close();
 	echo "</table>\n";
+}*/
+function day_list(){
+	$last_day = date("t", mktime(0,0,0,6,1,2015));
+	$start_week = date("w", strtotime(date("Y-m")."-01"));
+	$total_week = ceil(($last_day + $start_week) / 7);
+	$last_week = date('w', strtotime(date("Y-m")."-".$last_day));
+	
+	$day=1;
+    for($i=1; $i <= $total_week; $i++){
+		echo "<tr>";
+		for ($j=0; $j<7; $j++){
+			echo "<td height='30' align='center' bgcolor='#FFFFFF'>";
+			if (!(($i == 1 && $j < $start_week) || ($i == $total_week && $j > $last_week))){
+				if($j == 0){
+				echo "<font color='#FF0000'>";
+				}
+				else if($j == 6){
+					echo "<font color='#0000FF'>";
+				}
+				else{
+					echo "<font color='#000000'>";
+				}
+
+				if($day == date("j")){
+					echo "<b>";
+				}
+
+				echo "<input type='checkbox' name='day_list[]' id='day_list[]' value='".$day."'>$day";
+
+				if($day == date("j")){
+					echo "</b>";
+				}
+
+				echo "</font>";
+
+				$day++;
+			}
+			echo "</td>";
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
 }
 ?>
 <html>
@@ -39,10 +82,14 @@ function student_list(){
 <script type="text/javascript" src="../js/jquery-2.1.3.min.js"></script>
 <script type="text/javascript">
 function update_lecture(){
+	var checked = []
+	$("input[name='day_list[]']:checked").each(function (){
+		checked.push(parseInt($(this).val()));
+	});
 	$.ajax({
 		type: "POST",
 		url: "lecture_submit.php",
-		data: "lecture_title="+$('#lecture_title').val()+"&s_time="+$('#s_time').val()+"&e_time="+$('#e_time').val(),
+		data: "lecture_number="+$('#lecture_number').val()+"&lecture_title="+$('#lecture_title').val()+"&lecture_day="+checked+"&s_time="+$('#s_time').val()+"&e_time="+$('#e_time').val(),
 		cache: false,
 		success: function(data){
 			if(data == 0){
@@ -76,12 +123,29 @@ function update_student(){
 </script>
 </head>
 <body>
-과목명 : <input type="text" name="lecture_title" id="lecture_title" required />
-시작 시간 : <input type="time" name="s_time" id="s_time" value="09:00:00" required />
-끝 시간 : <input type="time" name="e_time" id="e_time" value="10:00:00" required />
-<input type="button" name="submit" id="submit" value="과목추가" onclick="update_lecture()"/><p/>
 과목 현황 : <?lecture_list()?>
-<?student_list()?>
+과목 번호 : <input type="text" name="lecture_number" id="lecture_number" required />
+과목 명 : <input type="text" name="lecture_title" id="lecture_title" required /><p/>
+<table width='500' cellpadding='0' cellspacing='1' bgcolor='#999999'>
+	<caption>강의 요일</caption>
+	<tr>
+		<td height='50' align='center' bgcolor='#FFFFFF' colspan='7'><?=date("Y년 n월")?></td>
+	</tr>
+	<tr>
+		<td height='30' align='center' bgcolor='#DDDDDD'>일</td>
+		<td align='center' bgcolor='#DDDDDD'>월</td>
+		<td align='center' bgcolor='#DDDDDD'>화</td>
+		<td align='center' bgcolor='#DDDDDD'>수</td>
+		<td align='center' bgcolor='#DDDDDD'>목</td>
+		<td align='center' bgcolor='#DDDDDD'>금</td>
+		<td align='center' bgcolor='#DDDDDD'>토</td>
+	</tr>
+	<?day_list()?>
+<p/>
+시작 시간 : <input type="time" name="s_time" id="s_time" value="09:00:00" required />
+강의 시간 : <input type="text" name="e_time" id="e_time" required /><p/>
+<input type="button" name="submit" id="submit" value="과목추가" onclick="update_lecture()"/><p/>
+<?//student_list()?>
 <p/>
 <input type="button" name="update" id="update" value="학생추가" onclick="update_student()"/>
 </body>
